2718040e2b1fbd5700a44dd3526653697ca6ec43
cmrivera <cmrivera0004@student.stcc.edu>
2020-08-31 20:33:05 -0400

    dependecies, server,index, data connected

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const projectsRouter = require("./projects/projects-router");
+const tasksRouter = require("./tasks/tasks-router");
+const resourcesRouter = require("./resources/resources-router");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.use("/projects", projectsRouter);
+server.use("/tasks", tasksRouter);
+server.use("/resources", resourcesRouter);
+
+server.use("/", (req, res) => {
+  res.json("Welcome!!");
+});
+
+module.exports = server;
